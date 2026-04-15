59090830466581a7408fb29c161790ba3f545736
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-04-15 19:50:56 +0100

    feat: add navigation between home and settings screens

--- lib/providers/screenProvider.dart
+++ lib/providers/screenProvider.dart
@@ -1,13 +1,24 @@
+import 'package:gifza/screens/settings.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:gifza/screens/home.dart';
 
-class ScreenProvider extends ChangeNotifier {
-  Widget get currentScreen => _currentScreen;
-  Widget _currentScreen = HomeScreen();
+enum AppScreen { home, settings }
 
-  routeToScreen({required Widget routeTo}) {
-    _currentScreen = routeTo;
+class ScreenProvider extends ChangeNotifier {
+  Widget get currentScreenWidget {
+    switch (currentScreen) {
+      case AppScreen.home:
+        return HomeScreen();
+      case AppScreen.settings:
+        return SettingsScreen();
+    }
+  }
+
+  AppScreen currentScreen = AppScreen.home;
+
+  routeToScreen({required AppScreen screen}) {
+    currentScreen = screen;
     notifyListeners();
   }
 }
--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gifza/providers/screenProvider.dart';
+
+import 'package:provider/provider.dart';
 
 class GifzaAppBar extends StatelessWidget {
   GifzaAppBar({super.key});
@@ -6,6 +9,7 @@ class GifzaAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final scheme = Theme.of(context).colorScheme;
+    final screenState = Provider.of<ScreenProvider>(context, listen: true);
     return Container(
         decoration: BoxDecoration(boxShadow: [
           BoxShadow(
@@ -15,10 +19,45 @@ class GifzaAppBar extends StatelessWidget {
               offset: Offset(0, 4))
         ]),
         child: AppBar(
+          toolbarHeight: 80,
           surfaceTintColor: Colors.transparent,
           backgroundColor: scheme.surface,
           elevation: 2,
           title: Row(children: [Text('Gifza', style: textTheme.titleMedium)]),
+          actions: [
+            screenState.currentScreen == AppScreen.home
+                ? IconButton(
+                    icon: Icon(Icons.settings),
+                    onPressed: () {
+                      screenState.routeToScreen(screen: AppScreen.settings);
+                    },
+                  )
+                : Row(
+                    children: [
+                      Icon(
+                        Icons.home,
+                        color: scheme.primary,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          screenState.routeToScreen(screen: AppScreen.home);
+                        },
+                        style: ButtonStyle(),
+                        child: Text(
+                          'Back To Home',
+                          style: TextStyle(
+                              color: scheme.primary, fontFamily: 'Vietnam'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30,
+                      )
+                    ],
+                  )
+          ],
         ));
   }
 }
