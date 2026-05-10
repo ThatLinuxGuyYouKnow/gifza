ea599558991036f8faa63691f79ae1748f5f6b35
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-05-10 23:00:13 +0100

    feat: restrict file picker to image types

--- lib/utils/pickAssetfromFiles.dart
+++ lib/utils/pickAssetfromFiles.dart
@@ -1,8 +1,10 @@
 import 'package:file_picker/file_picker.dart';
 
 pickAsset() async {
-  FilePickerResult? filePickerResult =
-      await FilePicker.pickFiles(withData: true);
+  FilePickerResult? filePickerResult = await FilePicker.pickFiles(
+      withData: true,
+      type: FileType.custom,
+      allowedExtensions: ['gif', 'jpg', 'jpeg', 'png', 'webp']);
 
   return filePickerResult;
 }
