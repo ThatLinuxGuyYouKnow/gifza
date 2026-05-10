2692911fbfefe4a7d9a8605f611f234c5b8ee3dc
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-05-10 12:39:08 +0100

    chore: remove install.sh script

--- assets/models/install.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-pip uninstall -y torch torchvision torchaudio executorch 2>/dev/null || true
-
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-pip install executorch open_clip_torch
